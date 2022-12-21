b40665ce78355e13675cdbb0e9d98da6868377ef
laxmi0703 <109914798+laxmi0703@users.noreply.github.com>
2022-12-21 20:17:47 +0530

    js practice

---
+++ array_method.js
@@ -0,0 +1,66 @@
+// var a = [10,20,30,40,50];
+// var b = a.includes(10);
+// console.log(b);
+
+// var c = a.push(100);
+// console.log(a);
+
+// var lax = a.pop();
+// console.log(a);
+
+
+// var q = a.shift();
+// console.log(a);
+
+// var y = a.unshift(10);
+// console.log(a);
+
+// var w = a.indexOf(40);
+// console.log(w);
+
+// var c = a.join("__");
+// console.log(c);
+
+// var v = a.toString();
+// console.log(v);
+
+// var n = a.reverse();
+// console.log(n);
+
+// var j = a.find((num) => num < 30)
+// console.log(j);
+
+// var lio = a.forEach((item) => item + 10)
+// console.log(a);
+
+// // let array2 = [10,20,30,40];
+// //  array2.forEach((element) => {
+// //     console.log (element * 10)
+// //  })
+
+// //  let array1 = [10,20,30];
+// //  array1.forEach((element) =>{
+// //     console.log(element + 100)
+// //  })
+
+//  let ary2 = [10,20,30,40,50];
+//  ary2.forEach((element)=>{
+//     console.log(element * 1000)
+//  })
+
+//  let art3 = [11,12,13,14,15];
+//  ary4 = art3.map((element)=>{
+//    console.log(element * 13 )
+//  })
+
+//  let ay6 = [100,200,300,400,500];
+//  ay7 = ay6.reduce((acc,num)=>acc+num ,0 )
+//  console.log(ay7)
+
+//  let ayu2 = [10,20,30,40];
+//  ar5 = ayu2.filter((element) => {
+//    console.log(element > 30)
+//  })
+
+
+
